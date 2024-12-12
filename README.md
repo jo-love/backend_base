b2a4566488e3362aede8eaa587c5c0b76b4fa01d
yeonjeongCho <jolove.dev@gmail.com>
2024-12-12 18:33:48 +0900

    nodemon 적용

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ## backend_base
  login-lecture
+
+#### noedenon - 자동으로 재시작
--- app/app.js
+++ app/app.js
@@ -8,6 +8,8 @@ const home = require('./src/routes/home');
 // 앱 세팅
 app.set('views', './src/views');
 app.set('view engine', 'ejs'); // html 느낌
+//  정적경로로 추가
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use('/', home); // use -> 미들웨어를 등록하는 메서드
 
